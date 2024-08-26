0e2488a58de414acc5d93c41483d53b6ee75663b
Franco Sancristobal <franco.sancris29@gmail.com>
2024-08-25 21:19:31 -0300

    add bootstrap

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 var db = require('./db');
 global.__root   = __dirname + '/'; 
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+/*
+Pegar en header de HTML lo siguiente para utilizar el Bootstrap
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+*/
 
 app.get('/api', function (req, res) {
   res.status(200).send('API works.');
