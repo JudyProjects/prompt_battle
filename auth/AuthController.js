5dbda769a38431a9914e432d3e85e5163751b2ce
Angelo <angelofestino5@gmail.com>
2024-08-29 17:06:00 -0300

    vista del login con token en cookies

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var VerifyToken = require('./VerifyToken');
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use(bodyParser.json());
-var User = require('../models/User.model');
-
-
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config');
-
+const path = require('path');
+var User = require('../models/User.model');
+var VerifyToken = require('./VerifyToken');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 router.post('/register', async function (req, res) {
 	var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -28,12 +26,6 @@ router.post('/register', async function (req, res) {
 		return res.status(500).send("Error");
 	}
 });
-
-router.get('/me2', VerifyToken, async function (req, res, next) {
-	const user =  await User.findById(req.userId, { password: 0 }); // projection
-			if (!user) return res.status(404).send("No existe el usuario.");
-			res.status(200).send(user);
-});
 router.post('/login',  async function (req, res) {
 	const user = await User.findOne({ email: req.body.email });
 		if (!user) return res.status(404).send('No existe usuario.');
@@ -42,7 +34,18 @@ router.post('/login',  async function (req, res) {
 		var token = jwt.sign({ id: user._id }, config.secret, {
 			expiresIn: 86400 // expira en 24 hours
 		});
-		res.status(200).send({ auth: true, token: token });
+		res.cookie('token', token, { sameSite: 'none', secure: true});
+		res.status(200).send({auth: true});
+});
+
+router.get('/login', function(req, res){
+	res.status(200).sendFile(path.join(__dirname, '../views/login.html'));
+})
+
+router.get('/me2', VerifyToken, async function (req, res, next) {
+	const user =  await User.findById(req.userId, { password: 0 }); // projection
+			if (!user) return res.status(404).send("No existe el usuario.");
+			res.status(200).send(user);
 });
 
 module.exports = router;
